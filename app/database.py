998619d00fd1398b62b0fd4f980bb323cdb880f0
Selvasuryaprakash <suryamass2110@gmail.com>
2025-07-08 23:47:12 +0530

    added database connections

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DB_URL = "postgresql://your_user:your_pass@localhost:5432/your_db"
+DB_URL = "postgresql://loclhost:root@localhost:5432/shoppinglive"
 
 engine = create_engine(DB_URL)
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
