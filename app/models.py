75d2429f02e7490cbcc3a5fd4f404957e86f4b56
Selvasuryaprakash <suryamass2110@gmail.com>
2025-07-09 00:25:20 +0530

    Users,role,privilege Tables models

--- app/models.py
+++ app/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, ForeignKey, Text
+from sqlalchemy import Column, Integer, String, ForeignKey, Text , ARRAY ,DateTime
 from sqlalchemy.orm import relationship
 from database import Base
 
@@ -6,31 +6,23 @@ from database import Base
 class User(Base):
     __tablename__ = "users"
 
-    id = Column(Integer, primary_key=True)
+    id = Column(String(25), primary_key=True)
     name = Column(String, nullable=False)
+    phone=Column(String(13),unique=True,nullable=False)
+    password=Column(String,nullable=False)
     email = Column(String, unique=True, nullable=False)
+    status=Column(Integer)
+    role = Column(ARRAY)
+    privilege = Column(ARRAY)
+    createddate = Column(DateTime)
 
-    posts = relationship("Post", back_populates="author", cascade="all, delete")
-    comments = relationship("Comment", back_populates="commenter", cascade="all, delete")
+class role(Base):
+    __tablename__ = "role"
+    id = Column(String(8))
+    privilege = Column(ARRAY)
 
-class Post(Base):
-    __tablename__ = "posts"
-
-    id = Column(Integer, primary_key=True)
-    title = Column(String, nullable=False)
-    content = Column(Text, nullable=False)
-    user_id = Column(Integer, ForeignKey("users.id"))
-
-    author = relationship("User", back_populates="posts")
-    comments = relationship("Comment", back_populates="post", cascade="all, delete")
-
-class Comment(Base):
-    __tablename__ = "comments"
-
-    id = Column(Integer, primary_key=True)
-    content = Column(Text, nullable=False)
-    user_id = Column(Integer, ForeignKey("users.id"))
-    post_id = Column(Integer, ForeignKey("posts.id"))
-
-    commenter = relationship("User", back_populates="comments")
-    post = relationship("Post", back_populates="comments")
+class privilege(Base):
+    __tablename__ = "privilege"
+    id = Column(String(8))
+    name = Column(String)
+    routeid = Column(String(8))
